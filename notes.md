2c76ac36ed0a344cebbf7a2138ee65a97f099ab5
Michael Lohr <michael@lohr.dev>
2022-11-21 19:56:19 +0100

    wip

--- notes.md
+++ notes.md
@@ -4,3 +4,9 @@ on docker container/inital setup
 https://github.com/berkowski/tokio-serial/issues/20#issuecomment-1114301487
 https://github.com/kumavale/sisterm/blob/269150ad7f008d112556f00691da0da7fcc2645c/src/color.rs
 https://github.com/kumavale/sisterm/blob/269150ad7f008d112556f00691da0da7fcc2645c/src/repl.rs
+
+
+rename to cereal monitor
+
+https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
+https://mdk.fr/blog/how-apt-does-its-fancy-progress-bar.html
--- src/main.rs
+++ src/main.rs
@@ -1,108 +1,17 @@
-use bytes::{BytesMut, Bytes};
-use color_eyre::eyre::{eyre, Result};
+use std::fmt::{self, Display};
+use std::io::Write;
+use std::{io::stdout, thread::sleep, time::Duration};
+
 use clap::{command, Parser, Subcommand};
-use crossterm::event::{EventStream, KeyCode, Event, KeyEvent, KeyModifiers};
-use futures::{StreamExt, FutureExt, select, SinkExt};
-use tokio_serial::{SerialPortType, SerialPortBuilderExt};
-use tokio_util::codec::{Decoder, Encoder, BytesCodec};
-use std::{env, io::{self, Write}, str};
-
-#[derive(Debug, Parser)]
-struct Args {
-    #[command(subcommand)]
-    subcommand: Action,
-}
-
-#[derive(Debug, Subcommand)]
-enum Action {
-    /// List the available serial ports
-    #[clap(alias="l")]
-    List,
-    /// Interactivly open a serial port
-    #[clap(alias="i")]
-    Interactive {
-        /// The serial port to open
-        port: String,
-        /// The baud rate to use
-        #[clap(short, long, default_value="9600")]
-        baud: u32,
-    },
-}
-
-#[tokio::main]
-async fn main() -> Result<()> {
-    color_eyre::install()?;
-    env_logger::init();
-
-    let args = Args::parse();
-
-    match args.subcommand {
-        Action::List => {
-           list_ports()?;
-        }
-        Action::Interactive { port, baud } => {
-            println!("Interactive mode");
-            interactive_mode(port).await?;
-        }
-    }
-
-    Ok(())
-}
-
-fn list_ports() -> Result<()> {
-    match tokio_serial::available_ports() {
-        Ok(ports) => {
-            match ports.len() {
-                0 => println!("No ports found."),
-                1 => println!("Found 1 port:"),
-                n => println!("Found {} ports:", n),
-            };
-            for p in ports {
-                println!("  {}", p.port_name);
-                match p.port_type {
-                    SerialPortType::UsbPort(info) => {
-                        println!("    Type: USB");
-                        println!("    VID:{:04x} PID:{:04x}", info.vid, info.pid);
-                        println!(
-                            "     Serial Number: {}",
-                            info.serial_number.as_ref().map_or("", String::as_str)
-                        );
-                        println!(
-                            "      Manufacturer: {}",
-                            info.manufacturer.as_ref().map_or("", String::as_str)
-                        );
-                        println!(
-                            "           Product: {}",
-                            info.product.as_ref().map_or("", String::as_str)
-                        );
-                        // waiting for the serialport 4.2.0 release (https://github.com/serialport/serialport-rs/issues/57)
-                        // println!(
-                        //     "         Interface: {}",
-                        //     info.interface
-                        //         .as_ref()
-                        //         .map_or("".to_string(), |x| format!("{:02x}", *x))
-                        // );
-                    }
-                    SerialPortType::BluetoothPort => {
-                        println!("    Type: Bluetooth");
-                    }
-                    SerialPortType::PciPort => {
-                        println!("    Type: PCI");
-                    }
-                    SerialPortType::Unknown => {
-                        println!("    Type: Unknown");
-                    }
-                }
-            }
-        }
-        Err(e) => {
-            eprintln!("{:?}", e);
-            eprintln!("Error listing serial ports");
-        }
-    }
-    Ok(())
-}
-
+use color_eyre::eyre::{eyre, Result};
+use crossterm::{cursor::*, csi, Command};
+use crossterm::terminal::SetSize;
+use crossterm::{
+    event::{Event, EventStream, KeyCode, KeyEvent, KeyModifiers},
+    execute,
+    style::{Print, ResetColor},
+    terminal::{self, Clear, ClearType},
+};
 
 // inspired by esp-rs/espflash
 struct RawModeGuard;
@@ -122,143 +31,57 @@ impl Drop for RawModeGuard {
     }
 }
 
-// inspired by dhylands/serial-monitor
-// Converts key events from crossterm into appropriate character/escape sequences which are then
-// sent over the serial connection.
-fn handle_key_event(key_event: KeyEvent) -> Result<Option<Bytes>> {
-    // The following escape sequeces come from the MicroPython codebase.
-    //
-    //  Up      ESC [A
-    //  Down    ESC [B
-    //  Right   ESC [C
-    //  Left    ESC [D
-    //  Home    ESC [H  or ESC [1~
-    //  End     ESC [F  or ESC [4~
-    //  Del     ESC [3~
-    //  Insert  ESC [2~
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct SetMargin(pub u16, pub u16);
 
-    let mut buf = [0; 4];
-
-    let key_str: Option<&[u8]> = match key_event.code {
-        KeyCode::Backspace => Some(b"\x08"),
-        KeyCode::Enter => Some(b"\r\n"), // CRLF
-        KeyCode::Left => Some(b"\x1b[D"),
-        KeyCode::Right => Some(b"\x1b[C"),
-        KeyCode::Home => Some(b"\x1b[H"),
-        KeyCode::End => Some(b"\x1b[F"),
-        KeyCode::Up => Some(b"\x1b[A"),
-        KeyCode::Down => Some(b"\x1b[B"),
-        KeyCode::Tab => Some(b"\x09"),
-        KeyCode::Delete => Some(b"\x1b[3~"),
-        KeyCode::Insert => Some(b"\x1b[2~"),
-        KeyCode::Esc => Some(b"\x1b"),
-        KeyCode::Char(ch) => {
-            if key_event.modifiers & KeyModifiers::CONTROL == KeyModifiers::CONTROL {
-                buf[0] = ch as u8;
-                if (ch >= 'a' && ch <= 'z') || (ch == ' ') {
-                    buf[0] &= 0x1f;
-                    Some(&buf[0..1])
-                } else if ch >= '4' && ch <= '7' {
-                    // crossterm returns Control-4 thru 7 for \x1c thru \x1f
-                    buf[0] = (buf[0] + 8) & 0x1f;
-                    Some(&buf[0..1])
-                } else {
-                    Some(ch.encode_utf8(&mut buf).as_bytes())
-                }
-            } else {
-                Some(ch.encode_utf8(&mut buf).as_bytes())
-            }
-        }
-        _ => None,
-    };
-    if let Some(key_str) = key_str {
-
-        if let Ok(val) = std::str::from_utf8(key_str) {
-            print!("{}", val);
-            std::io::stdout().flush()?;
-        }
-
-        Ok(Some(Bytes::copy_from_slice(key_str)))
-    } else {
-        Ok(None)
+impl Command for SetMargin {
+    fn write_ansi(&self, f: &mut impl fmt::Write) -> fmt::Result {
+        write!(f, csi!("{};{}r"), self.0, self.1)
     }
 }
 
-async fn monitor(stream: &mut tokio_serial::SerialStream) -> Result<()> {
-    let mut reader = EventStream::new();
-    let (rx_stream, tx_stream) = tokio::io::split(stream);
-
-    // TODO: writo own decoder similar to https://github.com/dhylands/serial-monitor/blob/master/src/string_decoder.rs to insert replacement characters
-    let mut serial_reader = tokio_util::codec::FramedRead::new(rx_stream, BytesCodec::new());
-    let mut serial_sink = tokio_util::codec::FramedWrite::new(tx_stream, BytesCodec::new());
-
-    loop {
-        let mut event = reader.next().fuse();
-        let mut serial_event = serial_reader.next().fuse();
-
-        select! {
-            maybe_event = event => {
-                match maybe_event {
-                    Some(Ok(event)) => {
-
-                        if event == Event::Key(KeyEvent::new(KeyCode::Char('c'), KeyModifiers::CONTROL)) {
-                            break;
-                        }
-                        if let Event::Key(key_event) = event {
-                            if let Some(key) = handle_key_event(key_event)? {
-                                serial_sink.send(key).await?;
-                            }
-                        } else {
-                            println!("Unrecognized Event::{:?}\r", event);
-                        }
-                    }
-                    Some(Err(e)) => println!("crossterm Error: {:?}\r", e),
-                    None => {
-                        println!("maybe_event returned None\r");
-                    },
-                }
-            },
-            maybe_serial = serial_event => {
-                match maybe_serial {
-                    Some(Ok(serial_event)) => {
-                        println!("Serial Event:{:?}\r", serial_event);
-                        // print!("{}", serial_event);
-                        std::io::stdout().flush()?;
-                    },
-                    Some(Err(e)) => {
-                        println!("Serial Error: {:?}\r", e);
-                        // This most likely means that the serial port has been unplugged.
-                        break;
-                    },
-                    None => {
-                        println!("maybe_serial returned None\r");
-                        break;
-                    },
-                }
-            },
-        };
+impl Display for SetMargin {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.write_ansi(f)
     }
-
-    Ok(())
 }
 
-async fn interactive_mode(port: String) -> Result<()> {
-    let mut port = tokio_serial::new(port, 9600).open_native_async()?;
-
-    // allowing multiple intances to connect at once, will "load balance" the incomming traffic. which is not what we want
-    // #[cfg(unix)]
-    // port.set_exclusive(false)
-    //     .expect("Unable to set serial port exclusive to false");
-
+fn main() -> Result<()> {
     let _raw_mode = RawModeGuard::new()?;
+    let mut stdout = stdout();
 
-    let result = monitor(&mut port).await;
-    // let mut reader = LineCodec.framed(port);
+    let (width, height) = terminal::size()?;
+    execute!(
+        stdout,
+        Print("\n"),
+        SavePosition,
+        SetMargin(0, height - 1),
+        RestorePosition,
+        MoveUp(1)
+    )?;
 
-    // while let Some(line_result) = reader.next().await {
-    //     let line = line_result.expect("Failed to read line");
-    //     println!("{}", line);
-    // }
+    for i in 0..20 {
+        sleep(Duration::from_millis(50));
+        execute!(
+            stdout,
+            SavePosition,
+            Print("Hello wordl!"),
+            MoveTo(0, height),
+            Print("bottom bar"),
+            RestorePosition,
+            Print("\n")
+        )?;
+    }
+
+    execute!(
+        stdout,
+        SavePosition,
+        SetMargin(0, height),
+        MoveTo(0, height),
+        Clear(ClearType::CurrentLine),
+        RestorePosition,
+        ResetColor,
+    )?;
 
     Ok(())
 }
